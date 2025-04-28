04d6b47da747c51a47526a6f3a3c7024f998ab8f
Justin Gélinas <6239428@cegepmontpetit.ca>
2025-04-28 17:23:11 -0400

    FCT - Ajout du init

---
+++ S09_Labo/Sql_Scripts/V1_2__VueChanteurs.sql
@@ -0,0 +1,15 @@
+
+	USE S09_Labo
+    GO
+-- Nouvelle vue
+	
+	--CREATE VIEW Musique.VW_ChanteurNbChansons AS
+	
+	---- ?
+	
+	--GO
+	
+	-- Résultat souhaité : id du chanteur, nom du chanteur, date de naissance et son nombre de chansons
+	
+	-- ChanteurID |Nom  | Date de naissance |Nombre de chansons
+	-- -----------|-----|-------------------|-------------------
---
+++ S09_Labo/Sql_Scripts/V1_3__ProcedureChanteurChanson.sql
@@ -0,0 +1,12 @@
+
+	USE S09_Labo
+	GO
+	-- Nouvelle procédure
+	
+	--CREATE PROCEDURE Musique.USP_ChanteurChansons
+	--@ChanteurID int
+	--AS
+	--BEGIN
+	--	-- ?
+	--END
+	GO
