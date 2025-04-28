403df0bfab71534f49056b6b6e78cdc23fcb52f4
Justin Gélinas <6239428@cegepmontpetit.ca>
2025-04-28 17:58:42 -0400

    FCT - V1.1 en cours

--- S09_Labo/Sql_Scripts/V1_1__CleArtificielleChanteur.sql
+++ S09_Labo/Sql_Scripts/V1_1__CleArtificielleChanteur.sql
@@ -1,13 +1,23 @@
 
+	USE S09_Labo
+	GO
 	-- Nouvelle colonne pour la PK de Musique.Chanteur et la FK de Musique.Chanson
-
-	
+	ALTER TABLE Musique.Chanteur ADD ChanteurID int
+	GO
+	ALTER TABLE Musique.Chanson ADD ChanteurID int
+	GO
 	-- Supprimer les anciennes contraintes FK puis PK (attention, l'ordre de suppression est important ici)
-	
+	ALTER TABLE Musique.Chanson DROP CONSTRAINT FK_Chanson_NomChanteur
+	GO
+	ALTER TABLE Musique.Chanteur DROP CONSTRAINT PK_Chanteur_Nom
+	GO
 
 	
 	-- Nouvelles contraintes PK puis FK
-	
+	ALTER TABLE Musique.Chanteur ADD CONSTRAINT PK_Chanteur_ChanteurID PRIMARY KEY (ChanteurID)
+	GO
+	ALTER TABLE Musique.Chanteur ADD CONSTRAINT FK_Chanson_ChanteurID FOREIGN KEY (ChanteurID) REFERENCES Musique.Chanteur (ChanteurID) ON DELETE CASCADE
+	GO
 
 	
 	-- Remplir la nouvelle colonne FK et faire en sorte que le nouveau champ que vous avez crée ChanteurID n'est pas null maintenant
