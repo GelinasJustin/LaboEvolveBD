88bf78eafd82af2efbbbffbf82d1322573213403
Justin Gélinas <6239428@cegepmontpetit.ca>
2025-05-08 11:06:36 -0400

    FCT - Finition de 1_1

--- S09_Labo/Sql_Scripts/V1_1__CleArtificielleChanteur.sql
+++ S09_Labo/Sql_Scripts/V1_1__CleArtificielleChanteur.sql
@@ -2,7 +2,7 @@
 	USE S09_Labo
 	GO
 	-- Nouvelle colonne pour la PK de Musique.Chanteur et la FK de Musique.Chanson
-	ALTER TABLE Musique.Chanteur ADD ChanteurID int
+	ALTER TABLE Musique.Chanteur ADD ChanteurID int Identity(1,1)
 	GO
 	ALTER TABLE Musique.Chanson ADD ChanteurID int
 	GO
@@ -14,17 +14,25 @@
 
 	
 	-- Nouvelles contraintes PK puis FK
+	ALTER TABLE Musique.Chanteur
+	ALTER COLUMN ChanteurID int NOT NULL
+	GO
 	ALTER TABLE Musique.Chanteur ADD CONSTRAINT PK_Chanteur_ChanteurID PRIMARY KEY (ChanteurID)
 	GO
-	ALTER TABLE Musique.Chanteur ADD CONSTRAINT FK_Chanson_ChanteurID FOREIGN KEY (ChanteurID) REFERENCES Musique.Chanteur (ChanteurID) ON DELETE CASCADE
+	ALTER TABLE Musique.Chanson ADD CONSTRAINT FK_Chanson_ChanteurID FOREIGN KEY (ChanteurID) REFERENCES Musique.Chanteur (ChanteurID) ON DELETE CASCADE
 	GO
 
 	
 	-- Remplir la nouvelle colonne FK et faire en sorte que le nouveau champ que vous avez crée ChanteurID n'est pas null maintenant
-	
-
-	
+	UPDATE Musique.Chanson
+	SET ChanteurID = (SELECT ChanteurID FROM Musique.Chanteur WHERE Nom = Musique.Chanson.NomChanteur)
+	GO
+	ALTER TABLE Musique.Chanson 
+	ALTER COLUMN ChanteurID int NOT NULL
+	GO
 	-- Supprimer l'ancienne colonne FK de Musique.Chanson (On veut garder le nom des chanteurs, donc on ne supprime pas l'ancienne PK !)
-	
+	ALTER TABLE Musique.Chanson
+	DROP COLUMN NomChanteur
+	GO
 
 	
